99221e51388fce168588dbae105b6aec5610a222
Ali-Hassanii <Ali-A.M.Hassani@outlook.com>
2025-01-27 15:38:39 +0400

    [fix] Routes I made a variable as local and also added routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'features/treatment/view/treatment_list_page.dart';
+import 'features/add_treatment/add_treatment.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,17 +8,30 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: appTheme,
-      home: TreatmentListPage(),
+      title: "Medicine Reminder",
+      theme: _appTheme,
+      routes: _routes,
+      initialRoute: "treatmentListPage",
     );
   }
 
-  ThemeData get appTheme => ThemeData.from(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          dynamicSchemeVariant: DynamicSchemeVariant.vibrant,
-          contrastLevel: 1,
-          seedColor: Colors.teal,
-        ),
-      );
+  // ! App Theme
+  ThemeData get _appTheme {
+    return ThemeData.from(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        dynamicSchemeVariant: DynamicSchemeVariant.vibrant,
+        contrastLevel: 1,
+        seedColor: Colors.teal,
+      ),
+    );
+  }
+
+  // ! Add Routes Here
+  Map<String, WidgetBuilder> get _routes {
+    return {
+      "treatmentListPage": (context) => TreatmentListPage(),
+      "addTreatmentPage": (context) => AddTreatmentPage(),
+    };
+  }
 }
