446ef204aebb543f5f38a1add3b3c69002330d4d
Ali-Hassanii <Ali-A.M.Hassani@outlook.com>
2025-02-07 19:22:16 +0330

    refactor: removing routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'features/treatment/view/treatment_list_page.dart';
-import 'features/add_treatment/add_treatment.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -10,8 +9,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "Medicine Reminder",
       theme: _appTheme,
-      routes: _routes,
-      initialRoute: "treatmentListPage",
+      home: const TreatmentListPage(),
     );
   }
 
@@ -20,18 +18,8 @@ class App extends StatelessWidget {
     return ThemeData.from(
       useMaterial3: true,
       colorScheme: ColorScheme.fromSeed(
-        dynamicSchemeVariant: DynamicSchemeVariant.vibrant,
-        contrastLevel: 1,
         seedColor: Colors.teal,
       ),
     );
-  }
-
-  // ! Add Routes Here
-  Map<String, WidgetBuilder> get _routes {
-    return {
-      "treatmentListPage": (context) => TreatmentListPage(),
-      "addTreatmentPage": (context) => AddTreatmentPage(),
-    };
   }
 }
